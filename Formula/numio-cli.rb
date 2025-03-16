e764fe508d7c375a89e25784612fe200bdcb837f
Sasha Jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-03-16 22:45:00 +0200

    Create numio-cli.rb

---
+++ Formula/numio-cli.rb
@@ -0,0 +1,15 @@
+class NumioCLI < Formula
+  desc "Numio CLI is a command-line tool written in Swift to perform time calculations."
+  homepage "https://github.com/neholos/numio-cli"
+  url "https://github.com/neholos/numio-cli/releases/download/v1.0.0/numio-cli-1.0.0-macOS.zip"
+  sha256 "5ef6083204e4dc2ab77f367ca78f4d296d4a599420d6f4fc85b433a89e2ce76e"
+  version "1.0.0"
+
+  def install
+    bin.install "numio-cli"
+  end
+
+  test do
+    system "#{bin}/numio-cli", "--version"
+  end
+end
